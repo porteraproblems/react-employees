555b4a276a43d0f692a3cc373c5ebeea3703f237
Alexander Portera <aportera3@gmail.com>
2021-01-21 14:18:52 -0500

    ughhh

---
+++ client/src/controllers/controllers.js
@@ -0,0 +1,31 @@
+const db = require("../models");
+
+// Defining methods for the employeeController
+module.exports = {
+    findAll: function (req, res) {
+        db.Employee
+            .find(req.query)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    filtered: function (req, res) {
+        console.log("Filtered called", req.params.name);
+        db.Employee
+            .find(req.query)
+            .where({ lastName: req.params.name })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    sorted: function (req, res) {
+        db.Employee
+            .find(req.query)
+            .sort({ lastName: req.params.order })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    findById: function (req, res) {
+        db.Employee
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
