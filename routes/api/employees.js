141b05b51709dacee827c2b91f11a8e2d5fdbee9
Alexander Portera <aportera3@gmail.com>
2021-01-21 23:12:56 -0500

    adding stuff

---
+++ routes/api/employees.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const employeeController = require("../../controllers/employeeController");
+
+// Matches with "/api/employees"
+router.route("/")
+    .get(employeeController.findAll)
+
+router.route("/filter/:name")
+    .get(employeeController.filtered)
+
+router.route("/sort/:order")
+    .get(employeeController.sorted)
+
+// Matches with "/api/employees/:id"
+router
+    .route("/:id")
+    .get(employeeController.findById)
+
+module.exports = router;
---
+++ scripts/seedDs.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+const faker = require("faker");
+
+// This file empties the employee collection and inserts 20 random employees
+
+mongoose.connect(
+    process.env.MONGODB_URI ||
+    "mongodb://localhost/employeesearch"
+);
+
+const employeeSeed = [];
+let empNumber = 20;
+
+for (let i = 0; i < empNumber; i++) {
+    employeeSeed.push({
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        phone: faker.phone.phoneNumber(),
+        dob: faker.date.past(),
+        department: faker.commerce.department(),
+        quote: faker.lorem.sentence()
+    })
+}
+
+db.Employee
+    .remove({})
+    .then(() => db.Employee.collection.insertMany(employeeSeed))
+    .then(data => {
+        console.log(data.result.n + " records inserted!");
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
